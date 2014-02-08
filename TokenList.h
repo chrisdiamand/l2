a78c428ebf50720ed717e4929fe50cd508f19da7
Chris Diamand <chris@diamand.org>
2014-02-08 15:12:14 +0000

    Added license headers and COPYING file.

--- TokenList.h
+++ TokenList.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Chris Diamand
+ * Copyright (c) 2014 Chris Diamand
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
