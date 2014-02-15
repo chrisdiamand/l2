358f240f1b8669b9d6736aa305dac776630f79fd
Chris Diamand <chris@diamand.org>
2014-02-15 23:40:02 +0000

    Use g++ compatible options.

--- project.py
+++ project.py
@@ -4,8 +4,9 @@ src.append("InputSource.cc")
 src.append("ParseTree.cc")
 src.append("TokenList.cc")
 
-CFLAGS = "-Wall -pedantic -std=c++11 "
-CFLAGS += "-Wno-deprecated-writable-strings"
+CFLAGS = "-Wall -pedantic"
+CFLAGS += " -std=c++0x"
+CFLAGS += " -Wno-write-strings"
 
 lang = CProg("l2", src, flags = CFLAGS)
 
